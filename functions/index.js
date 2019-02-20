505b936b4835f6da5602c6e0582c38a57e724a20
7nohe <7nohe.urata@gmail.com>
2019-02-20 18:02:24 +0900

    add new function

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,9 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.makeUppercase = functions.firestore.document('todo').onCreate((snap, context) => {
+  const newValue = snap.data();
+  const username = newValue.email.split('@')[0]
+  snap.ref.set({ username }, { merge: true })
+});
