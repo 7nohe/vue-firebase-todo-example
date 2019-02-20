938387aa89d35e4061b9667ce2e58f480ae2d9c1
DAIKI URATA <daiki@DAIKInoMacBook-Pro.local>
2019-02-20 19:20:09 +0900

    :sparkles: add unit tests with firestore emulator

--- tests/unit/Todo.spec.js
+++ tests/unit/Todo.spec.js
@@ -1,6 +1,7 @@
 import { createLocalVue, shallowMount } from "@vue/test-utils";
 import TodoApp from "../../src/components/TodoApp.vue";
 import TodoList from "../../src/components/TodoList.vue";
+import TodoForm from "../../src/components/TodoForm.vue";
 import Buefy from "buefy";
 
 const localVue = createLocalVue();
@@ -10,28 +11,27 @@ localVue.use(Buefy);
 const $firebase = {
   firestore: () => ({
     collection: () => ({
-      onSnapshot: () => null
+      onSnapshot: () => null,
+      add: () => null
     })
   })
 };
 
 describe("TodoApp", () => {
   test("should be Vue Instance", () => {
-    const wrapper = shallowMount(TodoApp, { localVue, mocks: { $firebase } });
+    const wrapper = shallowMount(TodoApp, { localVue });
     expect(wrapper.isVueInstance()).toBe(true);
   });
 });
 
 describe("TodoList", () => {
   test("should have two todo items", () => {
-    const wrapper = shallowMount(TodoList, {
-      localVue,
-      propsData: {
-        todos: [
-          { id: 1, todo: "読書", description: "一時間読書する" },
-          { id: 2, todo: "ジョギング", description: "一時間ジョギングする" }
-        ]
-      }
+    const wrapper = shallowMount(TodoList, { localVue, mocks: { $firebase } });
+    wrapper.setData({
+      todos: [
+        { id: 1, todo: "読書", description: "一時間読書する" },
+        { id: 2, todo: "ジョギング", description: "一時間ジョギングする" }
+      ]
     });
     const boxes = wrapper.findAll(".box");
     expect(boxes.length).toBe(2);
