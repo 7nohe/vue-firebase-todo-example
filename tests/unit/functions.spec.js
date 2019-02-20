9231553429a72ca831ad293ac946817203707050
DAIKI URATA <daiki@DAIKInoMacBook-Pro.local>
2019-02-21 08:31:50 +0900

    :sparkles: install sinon

--- tests/unit/functions.spec.js
+++ tests/unit/functions.spec.js
@@ -1,14 +1,27 @@
-import myFunction from '../../functions/index'
+import myFunctions from "../../functions/index";
+
+const fft = require("firebase-functions-test")();
 
 describe("helloworld", () => {
   test("should get 200 response", () => {
-    const req = { query: {text: 'input'} };
+    const req = { query: { text: "input" } };
     const res = {
-      send: (result) => {
-        expect(result).toBe('Hello from Firebase!');
+      send: result => {
+        expect(result).toBe("Hello from Firebase!");
       }
     };
-    
-    myFunction.helloWorld(req, res)
+
+    myFunctions.helloWorld(req, res);
   });
 });
+
+// describe("createUsername", () => {
+//   test("shoud create username from email", () => {
+//     const snap = fft.firestore.makeDocumentSnapshot(
+//       { email: "7nohe@example.com" },
+//       "user/test-user-id"
+//     );
+//     const wrapped = fft.wrap(myFunctions.createUsername);
+//     wrapped(snap, { params: { userId: "user-id-12345" } });
+//   });
+// });
